437d6a7a6544a47e91a625f075465ede9e3fabc8
Izanagi05 <izaaaajaa@gmail.com>
2022-12-20 14:58:49 +0700

    belajar penggabungan array

---
+++ MentorRYAN/pertemuan2/gabungkanarray.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "hello world";
+$makanan = array("Rumput", "Ikan", "Daging");
+$hewan = array("Sapi", "Kucing", "Singa");
+
+$gabungngan = array_merge($makanan, $hewan);
+print_r ($gabungngan)
+?>
