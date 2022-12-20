6e20accf034e54262638e018a315abec0ac300df
Izanagi05 <izaaaajaa@gmail.com>
2022-12-20 15:45:50 +0700

    ubah syintak manipualsistring 2

--- MentorRYAN/pertemuan2/manipulasiString.php
+++ MentorRYAN/pertemuan2/manipulasiString.php
@@ -1,8 +1,5 @@
 <?php
 
-    $banteng = [
-        "Aku","Suka","Partai","Banteng","Tapi","Boong"
-    ];
-    unset($banteng[4], $banteng[5]);
-    print_r($banteng);
+    $banteng = "Aku suka partai banteng";
+    echo substr(0, 23);
 ?>
